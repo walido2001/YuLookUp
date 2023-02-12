d11dc5c2a1c835db8f8116ca17849f1411292606
david <david.zinkiv@gmail.com>
2023-02-11 23:15:30 -0500

    implemented Majors class; gui displays courses of Civil Engineering; modified Course class

--- Project/src/main/java/courseStructures/Major.java
+++ Project/src/main/java/courseStructures/Major.java
@@ -1,25 +1,87 @@
 package courseStructures;
 
+import courseScraper.mainScraper;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Major {
+    private LinkedHashMap<String, Course> courses;
     private String name;
     private String[] mechCourses;
     private String[] testCourses;
-    private ArrayList<Course> courses;
+    private String[] civilCourses;
+    private LinkedHashMap<Integer, ArrayList<Course>> civilMajor;
 
-    public HashMap<String, Course> convertToHashMap(ArrayList<Course> list){
-        HashMap<String, Course> hashMap = new HashMap<>();
-        for (int i= 0; i<list.size(); i++) {
-            String courseCode = list.get(i).getCode();
-            hashMap.put(courseCode.substring(3, courseCode.length()-5), list.get(i));
-        }
-        return hashMap;
-    };
 
     public Major(String name) {
+        setAllCourses(mainScraper.getCourseList());
         this.name = name;
+        this.civilMajor = new LinkedHashMap<>();
+        this.civilCourses = new String[]{
+                "CHEM 1100",
+                "EECS 1011",
+                "EECS 1021",
+                "ENG 1101",
+                "ENG 1102",
+                "ESSE 1012",
+                "MATH 1013",
+                "MATH 1014",
+                "MATH 1025",
+                "PHYS 1800",
+                "PHYS 1801",
+                "CIVL 2000",
+                "CIVL 2120",
+                "CIVL 2150",
+                "CIVL 2160",
+                "CIVL 2210",
+                "CIVL 2220",
+                "CIVL 2240",
+                "ENG 2001",
+                "ENG 2003",
+                //"ESSE 2635",  //doesn't exist in .json
+                "MATH 2015",
+                "MATH 2271",
+                "MATH 2930",
+                "CIVL 3110",
+                "CIVL 3120",
+                "CIVL 3130",
+                "CIVL 3140",
+                "CIVL 3160",
+                "CIVL 3210",
+                "CIVL 3220",
+                "CIVL 3230",
+                "CIVL 3240",
+                "CIVL 3260",
+                "ENG 3000",
+                //"ENVS 2150", //doesn't exist in .json
+                "ESSE 2210",
+                "CIVL 4110",
+                "CIVL 4210",
+                "CIVL 4000",
+                "CIVL 4001",
+                "CIVL 4002",
+                "CIVL 4003",
+                //"CIVL 4004", //doesn't exist in .json
+                "CIVL 4011",
+                //"CIVL 4012",  //doesn't exist in .json
+                "CIVL 4013",
+                //"CIVL 4015",
+                //"CIVL 4016",
+                "CIVL 4021",
+                "CIVL 4022",
+                //"CIVL 4023",
+                //"CIVL 4024",
+                "CIVL 4031",
+                "CIVL 4033",
+                "CIVL 4034",
+                "CIVL 4041",
+                "CIVL 4042",
+                "CIVL 4043",
+                //"CIVL 4044",
+        };
         this.testCourses = new String[]{
                 "GH 1001",
                 "GH 1002",
@@ -89,6 +151,47 @@ public class Major {
                 "MECH 4512",
                 "ENG 4650"
         };
+
+        this.civilMajor = setMajor(civilCourses);
+    }
+
+    public LinkedHashMap<Integer, ArrayList<Course>> setMajor(String[] majorCourses){
+        LinkedHashMap<Integer, ArrayList<Course>> major = new LinkedHashMap<>();
+        ArrayList<Course> level1 = new ArrayList<>();
+        ArrayList<Course> level2 = new ArrayList<>();
+        ArrayList<Course> level3 = new ArrayList<>();
+        ArrayList<Course> level4 = new ArrayList<>();
+
+        for(int i = 0; i<majorCourses.length; i++){
+            Course course = courses.get(majorCourses[i]);
+            if(course.getLevel()==1) level1.add(course);
+            else if(course.getLevel()==2) level2.add(course);
+            else if(course.getLevel()==3) level3.add(course);
+            else if(course.getLevel()==4) level4.add(course);
+        }
+        major.put(1, level1);
+        major.put(2, level2);
+        major.put(3, level3);
+        major.put(4, level4);
+        return major;
+    }
+
+    public LinkedHashMap<Integer, ArrayList<Course>> getMajor(){
+        if(name.equals("Civil Engineering")){
+            return civilMajor;
+        }
+        return null;
+    }
+
+    public void setAllCourses(ArrayList<Course> list){
+        LinkedHashMap<String, Course> hashMap = new LinkedHashMap<>();
+        for (int i= 0; i<list.size(); i++)
+            hashMap.put((list.get(i).getCode()), list.get(i));
+        this.courses = hashMap;
+    }
+
+    public String[] getCivilCourses() {
+        return civilCourses;
     }
 
     public String getName() {
@@ -98,5 +201,5 @@ public class Major {
     public String[] getMajorCourses(){
         if(this.name.equals("Test")) return testCourses;
         return mechCourses;
-    };
+    }
 }
--- Project/src/main/java/gui/CourseTableGui.java
+++ Project/src/main/java/gui/CourseTableGui.java
@@ -14,17 +14,20 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 
 // following the tutorial found at https://docs.oracle.com/javafx/2/layout/LayoutSample.java.html
 
 
 public class CourseTableGui extends Application{
-    ArrayList<Course> courses = mainScraper.getCourseList();
+    Major civilEngineering = new Major("Civil Engineering");
+
     ArrayList<Button> buttonsYear1 = new ArrayList<>();
     ArrayList<Button> buttonsYear2 = new ArrayList<>();
     ArrayList<Button> buttonsYear3 = new ArrayList<>();
     ArrayList<Button> buttonsYear4 = new ArrayList<>();
+
     ArrayList<Course> year1courses;
     ArrayList<Course> year2courses;
     ArrayList<Course> year3courses;
@@ -36,66 +39,52 @@ public class CourseTableGui extends Application{
 
     @Override
     public void start(Stage primaryStage) {
-        Major mechMaj = new Major("Test");
-        HashMap<String, Course> courseMap = mechMaj.convertToHashMap(courses);
-
         primaryStage.setTitle("Courses By Major");
 
         BorderPane border = new BorderPane();
         HBox columns = new HBox();
-        VBox colYear1 = yearCol('1');
-        VBox colYear2 = yearCol('2');
-        VBox colYear3 = yearCol('3');
-        VBox colYear4 = yearCol('4');
+        VBox colYear1 = yearCol(1);
+        VBox colYear2 = yearCol(2);
+        VBox colYear3 = yearCol(3);
+        VBox colYear4 = yearCol(4);
 
         columns.getChildren().addAll(colYear1, colYear2, colYear3, colYear4);
 
         HBox hbox = majorsBar();
         border.setTop(hbox);
-        AnchorPane cols = new AnchorPane();
-        cols.getChildren().add(columns);
-        border.setCenter(cols);
+        border.setCenter(columns);
         primaryStage.setScene(new Scene(border, 500, 400));
         primaryStage.show();
     }
 
-    public VBox yearCol(char year){
+    public VBox yearCol(int year){
         VBox stack = new VBox();
         stack.setPadding(new Insets(15, 15, 15, 15));
         stack.setSpacing(10);
 
-        for(int i=0; i<5; i++){ ///courses.size()
-            Course course = courses.get(i);
-            String courseCode = course.getCode();
-            char level = courseCode.charAt(courseCode.length()-9);
-            Button btn = new Button("Button");
-            this.buttonsYear1.add(btn);
+        for(Course course : civilEngineering.getMajor().get(year)){
+            Button btn = new Button(course.getCode());
+            //this.buttonsYear1.add(btn);
             stack.getChildren().add(btn);
-
-//            if(level==year) {
-//                Button btn = new Button(courseCode);
-//                if (level == year && year == '1') {
-//                    this.year1courses.add(course);
-//                    this.buttonsYear1.add(btn);
-//                    stack.getChildren().add(btn);
-//                } else if (level == year && year == '2') {
-//                    this.year2courses.add(course);
-//                    this.buttonsYear2.add(btn);
-//                    stack.getChildren().add(btn);
-//                } else if (level == year && year == '3') {
-//                    this.year3courses.add(course);
-//                    this.buttonsYear3.add(btn);
-//                    stack.getChildren().add(btn);
-//                } else if (level == year && year == '4') {
-//                    this.year4courses.add(course);
-//                    this.buttonsYear4.add(btn);
-//                    stack.getChildren().add(btn);
-//                }
-//            }
         }
-
-        //stack.getChildren().addAll(/*aaaddddd*/);
-
+//
+//        for(Course course : civilEngineering.getMajor().get(1)){
+//            Button btn = new Button(course.getCode());
+//            this.buttonsYear2.add(btn);
+//            stack.getChildren().add(btn);
+//        }
+//
+//        for(Course course : civilEngineering.getMajor().get(2)){
+//            Button btn = new Button(course.getCode());
+//            this.buttonsYear3.add(btn);
+//            stack.getChildren().add(btn);
+//        }
+//
+//        for(Course course : civilEngineering.getMajor().get(3)){
+//            Button btn = new Button(course.getCode());
+//            this.buttonsYear4.add(btn);
+//            stack.getChildren().add(btn);
+//        }
         return stack;
     }
 
@@ -114,20 +103,23 @@ public class CourseTableGui extends Application{
         hbox.setSpacing(10);
         hbox.setStyle("-fx-background-color: #e31837;");
 
-        Button testMajButton = new Button("Test");
+        Button testMajButton = new Button("Civil");
         testMajButton.setPrefSize(100, 20);
 
-        Button mechMajButton = new Button("Mechanical");
-        mechMajButton.setPrefSize(100, 20);
+        Button compMajButton = new Button("Computer");
+        compMajButton.setPrefSize(100, 20);
 
         Button elecMajButton = new Button("Electrical");
         elecMajButton.setPrefSize(100, 20);
 
-        Button aeroMajButton = new Button("Aerospace");
-        aeroMajButton.setPrefSize(100, 20);
+        Button mechMajButton = new Button("Mechanical");
+        mechMajButton.setPrefSize(100, 20);
 
         Button softMajButton = new Button("Software");
         softMajButton.setPrefSize(100, 20);
+
+        Button aeroMajButton = new Button("Space");
+        aeroMajButton.setPrefSize(100, 20);
 
         hbox.getChildren().addAll(testMajButton, mechMajButton, elecMajButton, aeroMajButton, softMajButton);
 
