570281a6bc588b9cd16affd520452063f73d81a9
Walid AlDari <walidald@my.yorku.ca>
2023-02-12 16:44:03 -0500

    Added course Prerequisites in Course Class

--- Project/src/main/java/courseScraper/mainScraper.java
+++ Project/src/main/java/courseScraper/mainScraper.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import javax.crypto.spec.PSource;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
