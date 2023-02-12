e451ad3ba9552c9b3cd8475aee2dcd1c7612ecac
david <david.zinkiv@gmail.com>
2023-02-12 00:52:09 -0500

    gui displays courses only for Civil Engineering major;

--- Project/src/main/java/gui/CourseTableGui.java
+++ Project/src/main/java/gui/CourseTableGui.java
@@ -21,17 +21,13 @@ import java.util.LinkedHashMap;
 
 
 public class CourseTableGui extends Application{
-    Major civilEngineering = new Major("Civil Engineering");
+    Major major;
+    BorderPane border;
 
     ArrayList<Button> buttonsYear1 = new ArrayList<>();
     ArrayList<Button> buttonsYear2 = new ArrayList<>();
     ArrayList<Button> buttonsYear3 = new ArrayList<>();
     ArrayList<Button> buttonsYear4 = new ArrayList<>();
-
-    ArrayList<Course> year1courses;
-    ArrayList<Course> year2courses;
-    ArrayList<Course> year3courses;
-    ArrayList<Course> year4courses;
 
     public static void main(String[] args) {
         launch(args);
@@ -41,56 +37,49 @@ public class CourseTableGui extends Application{
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Courses By Major");
 
-        BorderPane border = new BorderPane();
-        HBox columns = new HBox();
-        VBox colYear1 = yearCol(1);
-        VBox colYear2 = yearCol(2);
-        VBox colYear3 = yearCol(3);
-        VBox colYear4 = yearCol(4);
-
-        columns.getChildren().addAll(colYear1, colYear2, colYear3, colYear4);
-
+        border = new BorderPane();
         HBox hbox = majorsBar();
         border.setTop(hbox);
-        border.setCenter(columns);
+        setCourseColumnsView();
         primaryStage.setScene(new Scene(border, 500, 400));
         primaryStage.show();
     }
 
-    public VBox yearCol(int year){
-        VBox stack = new VBox();
-        stack.setPadding(new Insets(15, 15, 15, 15));
-        stack.setSpacing(10);
+    public void setCourseColumnsView(){
+        border.setCenter(null);
+        if(major!=null) {
+            HBox columns = new HBox();
+            VBox colYear1 = yearCol(1, buttonsYear1);
+            VBox colYear2 = yearCol(2, buttonsYear2);
+            VBox colYear3 = yearCol(3, buttonsYear3);
+            VBox colYear4 = yearCol(4, buttonsYear4);
+            columns.getChildren().addAll(colYear1, colYear2, colYear3, colYear4);
+            columns.setHgrow(colYear1, Priority.ALWAYS);
+            columns.setHgrow(colYear2, Priority.ALWAYS);
+            columns.setHgrow(colYear3, Priority.ALWAYS);
+            columns.setHgrow(colYear4, Priority.ALWAYS);
+            border.setCenter(columns);
+        }
+    }
 
-        for(Course course : civilEngineering.getMajor().get(year)){
-            Button btn = new Button(course.getCode());
-            //this.buttonsYear1.add(btn);
+    public VBox yearCol(int year, ArrayList<Button> buttons){
+        VBox stack = new VBox();
+        stack.setPadding(new Insets(5, 15, 5, 15));
+        stack.setSpacing(5);
+
+        for(Course course : major.getMajor().get(year)){
+            Button btn = createButton(course.getCode(), 100, 30);
+            buttons.add(btn);
             stack.getChildren().add(btn);
         }
-//
-//        for(Course course : civilEngineering.getMajor().get(1)){
-//            Button btn = new Button(course.getCode());
-//            this.buttonsYear2.add(btn);
-//            stack.getChildren().add(btn);
-//        }
-//
-//        for(Course course : civilEngineering.getMajor().get(2)){
-//            Button btn = new Button(course.getCode());
-//            this.buttonsYear3.add(btn);
-//            stack.getChildren().add(btn);
-//        }
-//
-//        for(Course course : civilEngineering.getMajor().get(3)){
-//            Button btn = new Button(course.getCode());
-//            this.buttonsYear4.add(btn);
-//            stack.getChildren().add(btn);
-//        }
         return stack;
     }
 
-    public Button createButton(String text){
-        Button btn = new Button();
-        btn.setText(text);
+    public Button createButton(String text, double width, double height){
+        Button btn = new Button(text);
+//        btn.setMinWidth(width);
+//        btn.setMinHeight(height);
+        btn.setMaxWidth(Double.MAX_VALUE);
         return btn;
     }
 
@@ -103,25 +92,49 @@ public class CourseTableGui extends Application{
         hbox.setSpacing(10);
         hbox.setStyle("-fx-background-color: #e31837;");
 
-        Button testMajButton = new Button("Civil");
-        testMajButton.setPrefSize(100, 20);
+        Button civilMajButton = new Button("Civil");
+        civilMajButton.setPrefSize(100, 20);
+        civilMajButton.setOnAction(event -> {
+                major = new Major("Civil Engineering");
+                setCourseColumnsView();
+                });
 
         Button compMajButton = new Button("Computer");
         compMajButton.setPrefSize(100, 20);
+        compMajButton.setOnAction(event -> {
+            major = new Major("Computer Engineering");
+            setCourseColumnsView();
+        });
 
         Button elecMajButton = new Button("Electrical");
         elecMajButton.setPrefSize(100, 20);
+        elecMajButton.setOnAction(event -> {
+            major = new Major("Electrical Engineering");
+            setCourseColumnsView();
+        });
 
         Button mechMajButton = new Button("Mechanical");
         mechMajButton.setPrefSize(100, 20);
+        mechMajButton.setOnAction(event -> {
+            major = new Major("Mechanical Engineering");
+            setCourseColumnsView();
+        });
 
         Button softMajButton = new Button("Software");
         softMajButton.setPrefSize(100, 20);
+        softMajButton.setOnAction(event -> {
+            major = new Major("Software Engineering");
+            setCourseColumnsView();
+        });
 
         Button aeroMajButton = new Button("Space");
         aeroMajButton.setPrefSize(100, 20);
+        aeroMajButton.setOnAction(event -> {
+            major = new Major("Space Engineering");
+            setCourseColumnsView();
+        });
 
-        hbox.getChildren().addAll(testMajButton, mechMajButton, elecMajButton, aeroMajButton, softMajButton);
+        hbox.getChildren().addAll(civilMajButton, mechMajButton, elecMajButton, aeroMajButton, softMajButton);
 
         return hbox;
     }
