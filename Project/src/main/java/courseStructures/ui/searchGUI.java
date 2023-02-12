881deb6f6f01a8706916e6c2a7de884eb3b8aae2
Ali Saheb <alisaheb1234@gmail.com>
2023-02-12 13:19:57 -0500

    Added JavaDoc and removed dead code

--- Project/src/main/java/courseStructures/ui/searchGUI.java
+++ Project/src/main/java/courseStructures/ui/searchGUI.java
@@ -18,6 +18,11 @@ import javafx.stage.Stage;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 
+/**
+ * This is the controller class where the instance variables are
+ * declared. This class also includes all methods including the main
+ * and start methods.
+ */
 public class searchGUI extends Application { // controller class
 
     @FXML
@@ -31,13 +36,26 @@ public class searchGUI extends Application { // controller class
     @FXML
     private Label yuLabel;
 
+    /**
+     * This method returns what is inputted into the searchbar as a string
+     * @param event indicates that component defined action has occurred
+     * @return the text inside the searchbar as a string
+     */
     @FXML
-    void getUserInput(ActionEvent event) {
-        String input = searchBar.getText();
-        System.out.println(input);
+    public String getUserInput(ActionEvent event) {
+        System.out.println(searchBar.getText());
+        return searchBar.getText();
     }
 
-
+    /**
+     * This is the start method that creates the scene and sets
+     * the application window to visible.
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     * @throws IOException if the fxml file containing the UI data is not found
+     */
     @Override
     public void start(Stage stage) throws IOException {
 
@@ -49,6 +67,10 @@ public class searchGUI extends Application { // controller class
 
     }
 
+    /**
+     * This is the main method that launches the GUI
+     * @param args the command line arguments
+     */
     public static void main(String[] args) { // main class
         launch(args);
     }
