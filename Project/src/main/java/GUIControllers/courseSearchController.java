8df8b9e69545bb5572fca2b371d7e2cb726da399
Ali Saheb <alisaheb1234@gmail.com>
2023-02-13 21:16:59 -0500

    implemented getSearchResultsScene to switch between searchbar and results scene

--- Project/src/main/java/GUIControllers/courseSearchController.java
+++ Project/src/main/java/GUIControllers/courseSearchController.java
@@ -5,6 +5,7 @@ import gui.CourseTableGui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
--- Project/src/main/java/GUIControllers/mainController.java
+++ Project/src/main/java/GUIControllers/mainController.java
@@ -2,7 +2,9 @@ package GUIControllers;
 
 import gui.CourseTableGui;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,29 +15,35 @@ import java.io.IOException;
 
 public class mainController extends Application { // controller class
     private Stage window;
-    //    public AnchorPane paneMain;
-    //    public AnchorPane paneRed;
     public TextField searchBar;
     public Button homeSearchButton;
-    //    public Label yuLabel;
 
     @Override
     public void start(Stage stage) throws IOException {
         this.window = stage;
 
-        //        Parent homeView = FXMLLoader.load(getClass().getClassLoader().getResource("searchGUI.fxml"));
-        Parent searchView = FXMLLoader.load(getClass().getClassLoader().getResource("2ndCourseSearchView.fxml"));
-        //        CourseTableGui majorsListObject = new CourseTableGui();
-        //        Scene majorsListScene = majorsListObject.getMajorsListScene();
+        Parent searchView = FXMLLoader.load(getClass().getClassLoader().getResource("searchGUI.fxml"));
 
         Scene homeViewScene = new Scene(searchView);
-        //        Scene searchViewScene = new Scene();
 
         stage.setTitle("YULookUp");
         stage.setScene(homeViewScene);
         stage.show();
 
     }
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void getSearchResultsScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("searchReturnGUI.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
     public static void main(String[] args) { // main class
         launch(args);
