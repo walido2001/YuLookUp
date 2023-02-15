1432c1635936e02c6960dd0e652cd8307eaadfdb
Walid AlDari <walidald@my.yorku.ca>
2023-02-15 12:30:12 -0500

    Added test folder

---
+++ Project/src/test/java/courseSearchTest/courseSearchMethodsTest.java
@@ -0,0 +1,10 @@
+package courseSearchTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class courseSearchMethodsTest {
+    @Test
+    public void
+}
