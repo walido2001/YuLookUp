78ba4cd60e0faa019ce06e1e938641554dd26d46
Ali Saheb <alisaheb1234@gmail.com>
2023-02-15 12:55:33 -0500

    Implemented back button on gpa calculator

--- Project/src/main/java/gpaCalculator/gpaCalculator.java
+++ Project/src/main/java/gpaCalculator/gpaCalculator.java
@@ -1,17 +1,23 @@
 package gpaCalculator;
 
+import GUIControllers.courseSearchController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
+import java.io.IOException;
 import java.util.*;
 
 import java.net.URL;
@@ -171,6 +177,24 @@ public class gpaCalculator implements Initializable {
         textFieldCredits.setText(""+TotalCredits);
 
 
+    }
+
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+    public void getSearchResultsScene(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("searchReturnGUI.fxml"));
+        root = loader.load();
+
+        courseSearchController var = loader.getController();
+        var.setUserSearch("");
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
     }
 
 
