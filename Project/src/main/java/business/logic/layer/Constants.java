03e0bb1a932d41bd9aa6415d531094f51e9a7187
Walid AlDari <walidald@my.yorku.ca>
2023-03-09 15:11:34 -0500

    Minor Change

--- Project/src/main/java/business/logic/layer/Constants.java
+++ Project/src/main/java/business/logic/layer/Constants.java
@@ -6,7 +6,6 @@ public class Constants {
     //change to YOUR password for your local mysql
     public static final String PASSWORD = "1290";
 
-
     public static final String DATABASE = "yulookup";
     public static final String CONNECTION1 = "jdbc:mysql://localhost:3306/";
     public static final String CONNECTION = "jdbc:mysql://localhost:3306/" + DATABASE;
